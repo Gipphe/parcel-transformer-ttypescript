21aa60bbf2e009c6eca30eaa668669aa283b861b
Victor Nascimento Bakke <gipphe@gmail.com>
2023-02-14 14:16:52 +0100

    update code to newest implementation by Parcel
    See https://github.com/parcel-bundler/parcel/blob/v2/packages/transformers/typescript-tsc/src/TSCTransformer.js

--- src/TtscTransformer.ts
+++ src/TtscTransformer.ts
@@ -1,41 +1,55 @@
-import { Transformer } from '@parcel/plugin';
-import { loadTSConfig } from '@parcel/ts-utils';
-import typescript from 'ttypescript';
+import { Transformer } from "@parcel/plugin";
+import { loadTSConfig } from "@parcel/ts-utils";
+import typescript from "ttypescript";
+import SourceMap from "@parcel/source-map";
 
-const TtscTransformer = new Transformer({
-  async loadConfig({config, options}) {
-    return await loadTSConfig(config, options);
+export default new Transformer({
+  loadConfig({ config, options }) {
+    return loadTSConfig(config, options);
   },
 
-  async transform({asset, config}) {
-    asset.type = 'js';
+  async transform({ asset, config, options }) {
+    asset.type = "js";
 
-    let code = await asset.getCode();
-    let transpiled = typescript.transpileModule(
-      code,
-      {
-         compilerOptions: {
-           // React is the default. Users can override this by supplying their own tsconfig,
-           // which many TypeScript users will already have for typechecking, etc.
-           jsx: typescript.JsxEmit.React,
-           ...(config as object),
-           // Always emit output
-           noEmit: false,
-           // Don't compile ES `import`s -- scope hoisting prefers them and they will
-           // otherwise compiled to CJS via babel in the js transformer
-           module: typescript.ModuleKind.ESNext,
-         },
-         fileName: asset.filePath, // Should be relativePath?
-       },
-    );
+    let [code, originalMap] = await Promise.all([
+      asset.getCode(),
+      asset.getMap(),
+    ]);
 
-    return [
-      {
-        type: 'js',
-        content: transpiled.outputText,
+    let { outputText, sourceMapText } = typescript.transpileModule(code, {
+      compilerOptions: {
+        // React is the default. Users can override this by supplying their own tsconfig,
+        // which many TypeScript users will already have for typechecking, etc.
+        jsx: typescript.JsxEmit.React,
+        ...config,
+        // Always emit output
+        noEmit: false,
+        // Don't compile ES `import`s -- scope hoisting prefers them and they will
+        // otherwise compiled to CJS via babel in the js transformer
+        module: typescript.ModuleKind.ESNext,
+        sourceMap: Boolean(asset.env.sourceMap),
+        mapRoot: options.projectRoot,
       },
-    ];
+      fileName: asset.filePath, // Should be relativePath?
+    });
+
+    if (sourceMapText != null) {
+      outputText = outputText.substring(
+        0,
+        outputText.lastIndexOf("//# sourceMappingURL")
+      );
+
+      let map = new SourceMap(options.projectRoot);
+      map.addVLQMap(JSON.parse(sourceMapText));
+      if (originalMap) {
+        map.extends(originalMap);
+      }
+      asset.setMap(map);
+    }
+
+    asset.type = "js";
+    asset.setCode(outputText);
+
+    return [asset];
   },
 });
-
-export default TtscTransformer;
